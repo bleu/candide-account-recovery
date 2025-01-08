e4ef7348e5a3b03ba1054e9a729224af1e7045b3
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2025-01-08 17:48:43 -0300

    chore: create search input component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,11 @@
 "use client";
-import React, { useRef, useState } from "react";
+
+import React from "react";
 
 import { Button, Input } from "@bleu.builders/ui";
-import { Search } from "lucide-react";
-import { cn } from "@/lib/utils";
+import SearchInput from "./search-input";
 
 const Header = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [isFocused, setIsFocused] = useState(false);
-
   return (
     <header className="flex h-32 items-center justify-around gap-4 mx-8">
       <div className="flex flex-1 justify-center">
@@ -16,24 +13,7 @@ const Header = () => {
           Safe Account Recovery
         </h1>
       </div>
-      <div
-        className={cn(
-          "flex flex-1 items-center bg-content-background px-2 rounded-lg shadow-lg hover:cursor-text",
-          isFocused
-            ? "ring-2 ring-terciary ring-offset-background ring-offset-2"
-            : ""
-        )}
-        onClick={() => inputRef.current?.focus()}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-      >
-        <Search size={20} className="text-content-foreground opacity-60 mr-2" />
-        <Input
-          ref={inputRef}
-          className="flex-1 border-none bg-content-background text-primary text-xs font-medium py-3 font-roboto-mono focus:ring-0 focus:ring-offset-0 "
-          placeholder="Type address"
-        />
-      </div>
+      <SearchInput />
       <div className="flex flex-1 justify-center">
         <Button className="py-2 px-4 rounded-lg font-roboto-mono bg-primary text-primary-foreground hover:bg-terciary hover:text-primary">
           Connect wallet
---
+++ src/components/search-input.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+import { useRef, useState } from "react";
+import { Search } from "lucide-react";
+import { Input } from "@bleu.builders/ui";
+
+const SearchInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <div
+      className={cn(
+        "flex flex-1 items-center bg-content-background px-2 rounded-lg shadow-lg hover:cursor-text",
+        isFocused
+          ? "ring-2 ring-terciary ring-offset-background ring-offset-2"
+          : ""
+      )}
+      onClick={() => inputRef.current?.focus()}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+    >
+      <Search size={20} className="text-content-foreground opacity-60 mr-2" />
+      <Input
+        ref={inputRef}
+        className="flex-1 border-none bg-content-background text-primary text-xs font-medium py-3 font-roboto-mono focus:ring-0 focus:ring-offset-0 focus:text-primary focus:placeholder:text-primary"
+        placeholder="Type address"
+      />
+    </div>
+  );
+};
+
+export default SearchInput;

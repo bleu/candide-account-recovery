9897badafe201beea3d665a10f3fd520d38d4a40
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2025-01-08 17:58:11 -0300

    chore: add icon rotation on card hover

--- src/components/home-button.tsx
+++ src/components/home-button.tsx
@@ -7,7 +7,6 @@ interface HomeButtonProps {
   title: string;
   description: string;
   onClick?: () => void;
-  rotateIcon?: boolean;
   className?: string;
 }
 
@@ -16,23 +15,19 @@ const HomeButton = ({
   title,
   description,
   onClick,
-  rotateIcon,
   className,
 }: HomeButtonProps) => {
   return (
     <button
       className={cn(
-        "flex flex-col bg-content-background text-content-foreground p-5 rounded-lg shadow-lg w-full border border-content-background hover:border-primary text-start",
+        "flex flex-col bg-content-background text-content-foreground p-5 rounded-lg shadow-lg w-full border border-content-background hover:border-primary group text-start ",
         className
       )}
       onClick={onClick}
     >
       <div className="flex w-full justify-between mb-4">
         <span className="text-xs font-light opacity-60">{label}</span>
-        <MoveRight
-          className={cn("text-primary", rotateIcon && "-rotate-45")}
-          size={24}
-        />
+        <MoveRight className="text-primary group-hover:-rotate-45" size={24} />
       </div>
       <span className="mb-2 font-bold opacity-60">{title}</span>
       <p className="text-sm font-light opacity-60">{description}</p>

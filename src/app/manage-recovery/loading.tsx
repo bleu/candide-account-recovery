b7ae5073c7d81ed9461dbe17bdd246e70ea4a934
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2025-01-09 11:47:56 -0300

    feat: add loading spinner

---
+++ src/app/manage-recovery/loading.tsx
@@ -0,0 +1,5 @@
+import LoadingSpinner from "@/components/ui/loading-spinner";
+
+export default function Loading() {
+  return <LoadingSpinner className="flex flex-1 items-center justify-center" />;
+}
--- src/app/manage-recovery/page.tsx
+++ src/app/manage-recovery/page.tsx
@@ -1,16 +1,20 @@
 import { TextSearch } from "lucide-react";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 export default function ManageRecovery() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center mx-8">
-      <TextSearch size={100} className="text-foreground opacity-30" />
-      <h2 className="text-2xl text-primary font-bold font-roboto-mono text-center my-3">
-        Manage Recovery
-      </h2>
-      <p className="text-lg font-roboto-mono text-center text-foreground max-w-lg">
-        Search for an address to approve on going requests and manage guardians
-        permissions.
-      </p>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <div className="flex flex-col flex-1 items-center justify-center mx-8">
+        <TextSearch size={100} className="text-foreground opacity-30" />
+        <h2 className="text-2xl text-primary font-bold font-roboto-mono text-center my-3">
+          Manage Recovery
+        </h2>
+        <p className="text-lg font-roboto-mono text-center text-foreground max-w-lg">
+          Search for an address to approve on going requests and manage
+          guardians permissions.
+        </p>
+      </div>
+    </Suspense>
   );
 }

223c711bf91f6c675d2b78b85ff04aa5ecfe1d1a
Jean Neiverth <jeanneiverth@gmail.com>
2025-01-27 14:33:59 -0300

    feat: allow safe address override on useGuardians

--- src/hooks/useGuardians.ts
+++ src/hooks/useGuardians.ts
@@ -5,25 +5,27 @@ import { useAccount, useClient } from "wagmi";
 import { Address } from "viem";
 import { useQuery } from "@tanstack/react-query";
 
-export function useGuardians() {
+export function useGuardians(safeAddress?: Address) {
   const client = useClient();
   const account = useAccount();
+
+  const addressToSearch = safeAddress ?? account?.address;
 
   return useQuery({
     queryKey: ["guardians", account?.address, client?.transport.url],
     queryFn: async () => {
-      if (!account?.address || !client?.transport.url) {
+      if (!addressToSearch || !client?.transport.url) {
         throw new Error("Account or client transport URL not available");
       }
 
       const srm = new SocialRecoveryModule();
       const guardians = (await srm.getGuardians(
         client.transport.url,
-        account.address
+        addressToSearch
       )) as Address[];
 
       return guardians;
     },
-    enabled: Boolean(account?.address) && Boolean(client?.transport.url),
+    enabled: Boolean(addressToSearch) && Boolean(client?.transport.url),
   });
 }

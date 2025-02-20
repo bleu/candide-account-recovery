0b57d8b696a4ad3583536ae39b9e2f8d56427e82
Jean Neiverth <jeanneiverth@gmail.com>
2025-02-20 16:55:13 -0300

    feat: add wait for safe execution feature

--- src/hooks/useExecuteTransaction.ts
+++ src/hooks/useExecuteTransaction.ts
@@ -1,10 +1,12 @@
 "use client";
 
-import { useAccount, useWalletClient } from "wagmi";
+import { useAccount, usePublicClient, useWalletClient } from "wagmi";
 import { Address } from "viem";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "./use-toast";
 import { getReadableError } from "@/utils/get-readable-error";
+import { useIsSafeAccount } from "./use-is-safe-account";
+import { useWaitNextSafeAccountTx } from "./use-wait-next-safe-account-tx";
 
 interface BaseTx {
   to: Address;
@@ -23,6 +25,11 @@ export function useExecuteTransaction({
 }) {
   const { address: signer } = useAccount();
   const { data: walletClient } = useWalletClient();
+  const publicClient = usePublicClient();
+  const { isSafeAccount } = useIsSafeAccount();
+  const { waitNextSafeAccountTx } = useWaitNextSafeAccountTx({
+    safeAddress: signer,
+  });
 
   const mutation = useMutation({
     mutationFn: async () => {
@@ -37,6 +44,13 @@ export function useExecuteTransaction({
       for (const tx of txs) {
         const txHash = await walletClient.sendTransaction(tx);
         newTxHashes.push(txHash);
+        if (isSafeAccount) {
+          const successfulTx = await waitNextSafeAccountTx();
+          if (!successfulTx)
+            throw new Error("Timeout waiting for tx execution.");
+        } else {
+          await publicClient?.waitForTransactionReceipt({ hash: txHash });
+        }
       }
       return newTxHashes;
     },

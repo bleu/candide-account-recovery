ec3b16e637885cf32b70130bc6bee1116377a5d3
Jean Neiverth <jeanneiverth@gmail.com>
2025-03-24 16:15:37 -0300

    feat: add credits footer on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,54 @@
 "use client";
 
+import Footer from "@/components/footer";
 import HomeButton from "@/components/home-button";
 
 export default function Home() {
   return (
-    <main className="flex flex-1 items-center justify-center">
-      <div className="flex mx-12">
-        <div className="flex flex-col justify-center  gap-3 flex-1 ">
-          <h2 className="text-3xl font-bold text-primary font-roboto-mono">
-            Welcome to Safe <br /> Account Recovery
-          </h2>
-          <p className="font-light text-2xl font-roboto-mono">
-            Choose how you want to proceed.
-          </p>
-        </div>
-        <div className="flex-1 grid grid-cols-2 gap-12">
-          <HomeButton
-            label="OWNERS"
-            title="PROTECT MY ACCOUNT"
-            description="Activate Safe Account Recovery to protect your account."
-            href="/protect-account"
-            onClick={() => console.log("Protect account")}
-          />
-          <HomeButton
-            label="ANYYONE"
-            title="RECOVER AN ACCOUNT"
-            description="Ask for recovery if you lost access to your account."
-            onClick={() => console.log("Protect account")}
-            href="/ask-recovery"
-          />
-          <HomeButton
-            label="OWNERS AND GUARDIANS"
-            title="MANAGE RECOVERY"
-            description="Approve ongoing requests and manage guardians permissions."
-            href="/manage-recovery/dashboard"
-            onClick={() => console.log("Protect account")}
-          />
-          <HomeButton
-            label="OWNERS AND GUARDIANS"
-            title="CANCEL RECOVERY"
-            description="Cancel a request if there's no need to recover the account."
-            onClick={() => console.log("Protect account")}
-            href="/manage-recovery"
-          />
+    <main className="flex flex-1 flex-col h-min-screen justify-between">
+      <div className="flex flex-1 items-center justify-center">
+        <div className="flex mx-12">
+          <div className="flex flex-col justify-center  gap-3 flex-1 ">
+            <h2 className="text-3xl font-bold text-primary font-roboto-mono">
+              Welcome to Safe <br /> Account Recovery
+            </h2>
+            <p className="font-light text-2xl font-roboto-mono">
+              Choose how you want to proceed.
+            </p>
+          </div>
+          <div className="flex-1 grid grid-cols-2 gap-12">
+            <HomeButton
+              label="OWNERS"
+              title="PROTECT MY ACCOUNT"
+              description="Activate Safe Account Recovery to protect your account."
+              href="/protect-account"
+              onClick={() => console.log("Protect account")}
+            />
+            <HomeButton
+              label="ANYYONE"
+              title="RECOVER AN ACCOUNT"
+              description="Ask for recovery if you lost access to your account."
+              onClick={() => console.log("Protect account")}
+              href="/ask-recovery"
+            />
+            <HomeButton
+              label="OWNERS AND GUARDIANS"
+              title="MANAGE RECOVERY"
+              description="Approve ongoing requests and manage guardians permissions."
+              href="/manage-recovery/dashboard"
+              onClick={() => console.log("Protect account")}
+            />
+            <HomeButton
+              label="OWNERS AND GUARDIANS"
+              title="CANCEL RECOVERY"
+              description="Cancel a request if there's no need to recover the account."
+              onClick={() => console.log("Protect account")}
+              href="/manage-recovery"
+            />
+          </div>
         </div>
       </div>
+      <Footer />
     </main>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,74 @@
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import { SafeLogo } from "./ui/safe-logo";
+
+const Footer = () => {
+  return (
+    <footer className="w-full flex flex-wrap justify-around px-4 md:px-8 lg:px-16 xl:px-52 py-4 gap-8">
+      <CreditsCard
+        title="Frontend by"
+        imageUrl="/bleu-logo.png"
+        altText="Bleu Logo"
+        url="https://github.com/bleu"
+      />
+      <CreditsCard
+        title="Smart contracts by"
+        imageUrl="https://cdn.prod.website-files.com/6540e7cece6336bd77329344/65452258abad3510f9c0f72f_Camada_1.png"
+        altText="Candide Labs Logo"
+        url="https://github.com/candidelabs"
+      />
+      <CreditsCard
+        title="Audited by"
+        imageUrl="https://cdn.prod.website-files.com/6621233fd44f04553ba73645/6657486ee09d18c7e9abea2d_logo.svg"
+        altText="Ackee Logo"
+        url="https://github.com/candidelabs"
+      />
+      <CreditsCard
+        title="Formal verification"
+        content={<SafeLogo width={104} height={32} />}
+        altText="Safe logo"
+        url="https://github.com/candidelabs"
+      />
+    </footer>
+  );
+};
+
+interface CreditsCardProps {
+  title: string;
+  url: string;
+  imageUrl?: string;
+  altText?: string;
+  content?: ReactNode;
+}
+
+const CreditsCard = ({
+  title,
+  imageUrl,
+  altText,
+  content,
+  url,
+}: CreditsCardProps) => {
+  return (
+    <Link href={url} target="_blank" className="flex-shrink-0">
+      <div className="flex flex-col items-center justify-center gap-[6px] opacity-40 hover:opacity-100 transition-all">
+        <p className="opacity-80 font-medium text-center">{title}</p>
+        {imageUrl ? (
+          <div className="h-12 flex items-center justify-center">
+            <div className="w-32 h-10 relative flex items-center justify-center">
+              {/* eslint-disable-next-line */}
+              <img
+                src={imageUrl}
+                alt={altText || "Logo"}
+                className="max-w-full max-h-full object-contain"
+              />
+            </div>
+          </div>
+        ) : (
+          <div className="h-12 flex items-center justify-center">{content}</div>
+        )}
+      </div>
+    </Link>
+  );
+};
+
+export default Footer;

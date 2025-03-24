41f2e505b0ca1038e76ab3a328043118f88183ea
Jean Neiverth <jeanneiverth@gmail.com>
2025-03-24 16:42:51 -0300

    fix: credits links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,25 +9,25 @@ const Footer = () => {
         title="Frontend by"
         imageUrl="/bleu-logo.png"
         altText="Bleu Logo"
-        url="https://github.com/bleu"
+        url="https://github.com/bleu/candide-account-recovery"
       />
       <CreditsCard
-        title="Smart contracts by"
+        title="Smart Contracts by"
         imageUrl="https://cdn.prod.website-files.com/6540e7cece6336bd77329344/65452258abad3510f9c0f72f_Camada_1.png"
         altText="Candide Labs Logo"
-        url="https://github.com/candidelabs"
+        url="https://github.com/candidelabs/candide-contracts"
       />
       <CreditsCard
         title="Audited by"
         imageUrl="https://cdn.prod.website-files.com/6621233fd44f04553ba73645/6657486ee09d18c7e9abea2d_logo.svg"
         altText="Ackee Logo"
-        url="https://github.com/candidelabs"
+        url="https://github.com/candidelabs/candide-contracts/blob/main/audit/ackee-blockchain-candide-social-recovery-report.pdf"
       />
       <CreditsCard
         title="Formal verification"
         content={<SafeLogo width={104} height={32} />}
         altText="Safe logo"
-        url="https://github.com/candidelabs"
+        url="https://app.safe.global/welcome"
       />
     </footer>
   );
@@ -50,11 +50,11 @@ const CreditsCard = ({
 }: CreditsCardProps) => {
   return (
     <Link href={url} target="_blank" className="flex-shrink-0">
-      <div className="flex flex-col items-center justify-center gap-[6px] opacity-40 hover:opacity-100 transition-all">
-        <p className="opacity-80 font-medium text-center">{title}</p>
+      <div className="flex flex-col items-center justify-center gap-1 opacity-40 hover:opacity-100 transition-all">
+        <p className="opacity-80 font-medium text-center text-sm">{title}</p>
         {imageUrl ? (
           <div className="h-12 flex items-center justify-center">
-            <div className="w-32 h-10 relative flex items-center justify-center">
+            <div className="w-24 h-8 relative flex items-center justify-center">
               {/* eslint-disable-next-line */}
               <img
                 src={imageUrl}
